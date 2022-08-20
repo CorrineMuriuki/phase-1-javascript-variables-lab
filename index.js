7f475723ae015c5f3cf02391c690031c7591a1b9
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-08-20 06:21:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+let companyName = 'Scuber'; {
+const companyName = ["Expected companyName to be a const"];
+console.log =[companyName];
+}
+let mostProfitableNeighborhood= 'Chelsea'
+let companyCeo ='Susan Smith'
